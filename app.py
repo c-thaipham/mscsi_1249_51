764b603b7a266ed050035fa92ba4555d9b03893d
Pham <C-Thai.Pham@charter.com>
2022-04-27 08:23:26 -0500

    Search device by ip address

--- app.py
+++ app.py
@@ -39,11 +39,13 @@ basicAuthCredentials = (st.secrets["ovoc_username"], st.secrets["ovoc_password"]
 
 device_placeholder = "e.g. 11.11.111.111 or  +16012345678"
 devices = get_data(ems_system, default_api_endpoint)["devices"]
-calls = get_data(ems_system, calls_api_endpoint)["calls"]
-device = st.text_input("Search a Device", placeholder=device_placeholder)
+# calls = get_data(ems_system, calls_api_endpoint)["calls"]
+device = st.text_input("Find a Device", placeholder=device_placeholder)
 search_by = st.radio(
      "Search by",
      ('IP Address', 'Source Number', 'Destination Number'))
+    
+default_ip_address = "208.125.140.98"
     
 if search_by == 'IP Address':
     pass
@@ -52,26 +54,28 @@ if search_by in ['Source Number', 'Destination Number']:
 
 if st.button('Search'):
 
-    st.subheader(f"Search Results")
-    col1, col2, col3 = st.columns(3)
-    col1.metric("Devices", f"{len(devices)}")
-    col2.metric("Source Numbers", f"{0}")
-    col3.metric("Destination Numbers", "10")
+    # st.subheader(f"Search Results")
+    # col1, col2, col3 = st.columns(3)
+    # col1.metric("Devices", f"{len(devices)}")
+    # col2.metric("Source Numbers", f"{0}")
+    # col3.metric("Destination Numbers", "10")
 
-    for i, d in enumerate(devices):
-        if i < 5:
+    # Find IP Address of the device and display it
+    for d in devices:
+        device_api_endpoint = d["url"]
+        device_data = get_data(ems_system, device_api_endpoint)
+        ip_address = device_data["ipAddress"]
+                    
+        if ip_address == default_ip_address:
             with st.expander(f"{d['description']}"):
                 with st.container():
-                    # Find IP Address of the device and display it
-                    device_api_endpoint = d["url"]
-                    device_data = get_data(ems_system, device_api_endpoint)
-                    ip_address = device_data["ipAddress"]
                     st.subheader(f"IP Address: {ip_address}")
+                    break
 
 
                 # st.download_button("Download data", data=json.dumps(detailed_data), file_name="data.json", mime="text/json")
-    for i, c in enumerate(calls):
-        if i < 5:
-            call_api_endpoint = c["url"]
-            call_data = get_data(ems_system, call_api_endpoint)
-            st.json(call_data)
+    # for i, c in enumerate(calls):
+    #     if i < 5:
+    #         call_api_endpoint = c["url"]
+    #         call_data = get_data(ems_system, call_api_endpoint)
+    #         st.json(call_data)
