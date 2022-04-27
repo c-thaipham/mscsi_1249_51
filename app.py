93d8ae3a6f77e3733af5d55d47bda773d84844ab
Pham <C-Thai.Pham@charter.com>
2022-04-27 11:30:01 -0500

    Display call information

--- app.py
+++ app.py
@@ -103,7 +103,6 @@ if st.button('Search'):
                                     if call_num < 3:
                                         call_num += 1
                                         st.json(call_data)
-                                        continue
                                     break
                     break
 
