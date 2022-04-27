44dd4f1d327db2f5ee46a1eea457e7f40348ca74
Pham <C-Thai.Pham@charter.com>
2022-04-27 13:10:14 -0500

    Implement pandas

--- app.py
+++ app.py
@@ -82,4 +82,4 @@ if st.button('Search'):
         if not validate_ip_address(device):
             display_custom_text("Please enter a valid IP Address")
         else:
-            st.table(devices_series)
+            st.write(devices_series)
